3a9f1995a5b99c911b8b459743184690a5c7ff20
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-02-02 13:51:37 +0500

    1 v

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/cd2qeo9t0q1igh3g?svg=true)](https://ci.appveyor.com/project/nugmanov87/ahj-homeworks-task1)
---
+++ webpack.common.js
@@ -0,0 +1,59 @@
+const path = require('path'); // Node.js модуль для разрешения путей файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // устанавливается через npm
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: './src/favicon.ico',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
